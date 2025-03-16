8fcfd2c62cbbce70bf4531ec34a1d6ee641938c4
Ekaterina Volynkina <katerina.vakutina@gmail.com>
2025-03-16 10:42:03 +0300

    Добавила тест "Попытка удалить несуществующего питомца", попытка 4, удалила print в прошлом коммите и сейчас исправила deleted

--- tests/test_pet.py
+++ tests/test_pet.py
@@ -10,6 +10,6 @@ class TestPet:
         with allure.step("Отправка запроса на удаление несуществующего питомца"):
             response = requests.delete(url=f"{BASE_URL}/pet/9999")
         with allure.step("Проверка статуса ответа"):
-            assert response.status_code == 200, "Код ответа не совпал с ожидаемым"
+            assert response.status_code == 200, "Код ответа не совпал с ожидаемым!"
         with allure.step("Проверка текстового содержимого ответа"):
             assert response.text == ("Pet deleted"), "Текст ошибки не совпал с ожидаемым!"
